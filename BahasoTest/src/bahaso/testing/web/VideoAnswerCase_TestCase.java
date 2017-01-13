b061f799d6c513eafa1ea127295f5594e16e9bd7
Ade Mahendra Lubis <ademahendra@bahaso.com>
2017-01-13 17:26:19 +0700

    getAnswerData to VideoAnswerCase

--- BahasoTest/src/bahaso/testing/web/VideoAnswerCase_TestCase.java
+++ BahasoTest/src/bahaso/testing/web/VideoAnswerCase_TestCase.java
@@ -1,11 +1,19 @@
 package bahaso.testing.web;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
 
 import bahaso.testing.general.General;
 import bahaso.testing.webElement.LandingPage;
@@ -15,7 +23,8 @@ public class VideoAnswerCase_TestCase extends General{
 	LandingPage landingPage = null;
 	VideoAnswerCase videoAnswerCase = null;
 	HashMap<String, String> LoginData = new HashMap<String, String>();
-	String[] answer = {"Good morning, Kevin."};
+	ArrayList<WebElement> buttonPage = new ArrayList<WebElement>();
+	Object answer;
 	
 
 	@BeforeMethod
@@ -27,6 +36,18 @@ public class VideoAnswerCase_TestCase extends General{
 	  videoAnswerCase = new VideoAnswerCase(driver);
 	  landingPage.doLogin(LoginData);
 	  driver.get(baseUrl + "/ngeadmin/previewCaseNewTab/5680bf66938e8ec1788b4567");
+	  
+	  //Get Case ID
+	  buttonPage = (ArrayList<WebElement>) driver.findElements(By.className("btn-page"));
+	  
+	  //Get Data from Database
+	  MongoCollection<Document> table = db.getCollection("case");
+	  BasicDBObject searchQuery = new BasicDBObject();
+	  searchQuery.put("_id", new ObjectId(buttonPage.get(0).getAttribute("data-id")));
+	  Document cursor = table.find(searchQuery).first();
+	  
+	  //Convert answer
+	  answer = videoAnswerCase.getAnswerData(cursor);
   	}
 	
 	@Test

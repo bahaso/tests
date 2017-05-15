a227338ae90ce7820a563846f13abc0ffa6aff2d
ademahendralubis <ademahendralubis@gmail.com>
2017-05-15 14:56:35 +0700

    add test and automation for sentence formation

---
+++ BahasoTest/src/bahaso/testing/android/SentenceFormation_test.java
@@ -0,0 +1,73 @@
+package bahaso.testing.android;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import bahaso.testing.androidElement.Home;
+import bahaso.testing.androidElement.SentenceFormation;
+import bahaso.testing.androidElement.SplashScreen;
+import bahaso.testing.general.mobileGeneral;
+
+public class SentenceFormation_test extends mobileGeneral{
+	SplashScreen splashScreen;
+	Home home;
+	SentenceFormation sentenceFormation;
+	ArrayList<String> answers = new ArrayList<String>();
+	
+	@BeforeClass
+	public void beforeclass(){
+		answers.add("Their names");
+		answers.add("are Vince");
+		answers.add("and Derek");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod(){
+		try {
+		startEngine();
+		splashScreen = new SplashScreen(driver);
+		home = new Home(driver);
+		sentenceFormation = new SentenceFormation(driver);
+		Thread.sleep(4000);
+		action.press(home.getAllTypeCase(), 0, 0).release().perform();
+		Thread.sleep(4000);
+		driver.findElementById("com.bahaso:id/radioButtonIntroAndGeneralCase").click();
+		driver.findElementById("com.bahaso:id/buttonLoadTestingCase").click();
+		Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void test1(){
+		try {
+			sentenceFormation.answerRight(answers);
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void test2(){
+		try {
+			sentenceFormation.answerWrong(answers);
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@AfterMethod
+	public void afterMethod(){	
+		driver.quit();
+	}
+}	
